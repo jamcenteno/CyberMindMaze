bd9fa846aab8d440ea4bc66295457262d381ff66
jamcenteno <jameelahcenteno@gmail.com>
2020-03-06 19:11:31 +0800

    updating project files

---
+++ Assets/ThirdPersonInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ThirdPersonInput : MonoBehaviour
+{
+    public FixedJoystick LeftJoystick;
+    public FixedButton Button;
+    protected ThirdPersonUserControl Control;
+    public FixedTouchField TouchField;
+
+    protected float CameraAngle;
+    protected float CameraAngleSpeed = 0.2f;
+    //Start is called before the first frame update
+    void Start()
+    {
+        Control = GetComponent<ThirdPersonUserControl>();
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        Control.m_Jump = Button.Pressed;
+        Control.Hinput = LeftJoystick.Direction.x;
+        Control.Vinput = LeftJoystick.Direction.y;
+
+        CameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
+       
+
+        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(-0.8f, 2.7f, -2f);
+        Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
+
+        
+    }
+}
---
+++ Assets/wait.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class wait : MonoBehaviour
+
+{
+    public float waiting_time=7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Waiting());
+
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(waiting_time);
+        SceneManager.LoadScene(1);
+    }
+    
+}
+ 
